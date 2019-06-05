1f7d622bb1b5690f348b280b3811b4cc2bf3e961
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2019-06-06 00:27:00 +0200

    Bump release to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfavicon",
-    version="0.0.1",
+    version="0.0.2",
     author="Bilal Elmoussaoui",
     author_email="bil.elmoussaoui@gmail.com",
     description="Async favicon fetcher",
