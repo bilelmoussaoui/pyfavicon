8c1b7a8486abf0526e3e1e37cfb7ea4a19c20031
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2019-06-06 00:26:16 +0200

    First 0.0.1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfavicon",
-    version="0.0.2",
+    version="0.0.1",
     author="Bilal Elmoussaoui",
     author_email="bil.elmoussaoui@gmail.com",
     description="Async favicon fetcher",
